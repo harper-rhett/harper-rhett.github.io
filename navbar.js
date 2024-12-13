e711a0fb90448f56b5c888b8efff161d096b35d5
Harper Rhett <harperrhett@gmail.com>
2024-12-12 22:30:03 -0600

    Navigation bar reuse

--- navbar.js
+++ navbar.js
@@ -1,8 +1,17 @@
 fetch('navbar.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#navbar");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+
+        const currentPage = window.location.pathname.split('/').pop();
+        const links = newelem.querySelectorAll('a');
+        links.forEach(link => {
+            if (link.getAttribute('href') === currentPage) {
+                link.classList.add('active');
+            }
+        });
+
+        oldelem.parentNode.replaceChild(newelem, oldelem);
+    })
